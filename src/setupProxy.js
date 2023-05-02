adbcd45b6ba5547bc276a1cd1aef0be3e2e5ba1a
Emmanuel Ejim <emmanueleejim@gmail.com>
2023-05-02 04:05:59 -0500

    improved searchbar and the local hosting ports

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@
 
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const TARGET =
-  process.env.NODE_ENV === "development" ? "http://localhost:5000" : "";
+  process.env.NODE_ENV === "development" ? "http://localhost:5050" : "";
 module.exports = (app) => {
   app.use(
     "/api",
